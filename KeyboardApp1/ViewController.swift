f21be3e12e6ffd745033bb7a622c87f28a75bc5c
YutaroNakaji <yutaro.nakaji@four-brains.com>
2021-09-13 21:14:17 +0900

    Initial Commit

---
+++ KeyboardApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeyboardApp1
+//
+//  Created by Yutaro Nakaji（B） on 2021/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
